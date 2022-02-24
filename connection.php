f91ef5dcd453c3d2527b683453d911fbaca416d7
janwillem2020 <70693700+janwillem2020@users.noreply.github.com>
2022-02-24 21:42:23 +0100

    updated voor mamp

--- connection.php
+++ connection.php
@@ -2,9 +2,9 @@
 
 function openDatabase() {
     try {
-        return $conn = new PDO('mysql:host=localhost;dbname=to-do-list', "root", "mysql");
+        return $conn = new PDO('mysql:host=localhost;dbname=to-do-list', "root", "root");
     } catch (PDOException $e) {
-        print "Error!: " . $e->getMessage() . "<br/>";
+        print "Error! : " . $e->getMessage() . "<br/>";
         die();
     }
 }
